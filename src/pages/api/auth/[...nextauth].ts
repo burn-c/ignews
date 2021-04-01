5263063714bc56a85641a30b584f1f841a7188db
Carlos Oliveira <chopcsom@gmail.com>
2021-04-01 00:25:46 -0300

    feat: validation of existing user at login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -14,18 +14,34 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  jwt: {
-    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
-  },
+  // jwt: {
+  //   signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+  // },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email }}
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
 
