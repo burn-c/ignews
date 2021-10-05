f806113f7a94dea5dc0118eedc8fa95922cd2a3e
Carlos Oliveira <chopcsom@gmail.com>
2021-10-05 17:06:16 -0300

    test: adds SignInButton

---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react'
+import { mocked } from 'ts-jest/utils'
+import { useSession } from 'next-auth/client'
+import { SignInButton } from "./index"
+
+jest.mock('next-auth/client')
+
+describe('SignInButton component', () => {
+  const useSessionMocked = mocked(useSession)
+
+  it('renders correctly when user is not authentucated', () => {
+
+    useSessionMocked.mockReturnValueOnce([null, false])
+
+    render(
+      <SignInButton />
+    )
+
+    expect(screen.getByText('Sign in with Github')).toBeInTheDocument()
+  })
+
+  it('renders correctly when user is authentucated', () => {
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: 'John Doe',
+          email: 'john.doe@example.com'
+        },
+        expires: 'fake-expires'
+      },
+      false
+    ])
+
+
+    render(
+      <SignInButton />
+    )
+
+    expect(screen.getByText('John Doe')).toBeInTheDocument()
+  })
+})
