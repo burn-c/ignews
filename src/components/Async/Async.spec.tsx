074684189790360704c0c784a74da0525e810c06
Carlos Oliveira <chopcsom@gmail.com>
2022-07-07 23:18:37 -0300

    config(app): jest coverage report

--- src/components/Async/Async.spec.tsx
+++ src/components/Async/Async.spec.tsx
@@ -1,4 +1,4 @@
-import { findByText, render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import { Async } from "."
 
 describe('Async tests', () => {
